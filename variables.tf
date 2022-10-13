014275611cd14108d54421f2a8f8845b0dbf7c68
Ant Andrades <antandrades@gmail.com>
2022-10-13 17:24:39 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,24 +6,29 @@
 variable "table_name" {
   description = "The name to use for all the cluster resources"
   type        = string
+  default = null
 }
 
 variable "db_remote_state_bucket" {
   description = "The name of the S3 bucket used for the database's remote state storage"
   type        = string
+  default = null
 }
 
 variable "db_remote_state_key" {
   description = "The name of the key in the S3 bucket used for the database's remote state storage"
   type        = string
+  default = null
 }
 
 variable "read_capacity" {
   description = "The minimum number of EC2 Instances in the ASG"
   type        = number
+  default = null
 }
 
 variable "write_capacity" {
   description = "The maximum number of EC2 Instances in the ASG"
   type        = number
+  default = null
 }
